ab23cb61794b34eb541e619e595a5791a1e3a367
cbkremer <106339191+cbkremer@users.noreply.github.com>
2022-06-14 15:11:59 -0300

    Add files via upload

---
+++ stefanini/java/aulas/Aula3.java
@@ -0,0 +1,24 @@
+package com.stefanini.aprendendo.com.eclipse.aula;
+
+public class Aula3 {
+	byte numByte = 127; //8 bits
+	short numShort = 32767; // 16 bits
+	int numInt = 2147483647; // 32 bits
+	long numLong = 9223372036854770000l; //64 bits
+	
+	float numFloat = 10.023f; //32 bits
+	double numDouble = 1000000.234; //64 bits
+	
+	String texto = "Bem vindos devs";
+	Integer numeroConta = 4000;
+	Long numeroLongo = 123l;
+	
+	//Ele deve comecar com uma letra minuscula
+	//Não deve começar com caracteres especiais
+	//Se o nome for composto, primeira com letra minuscula segunda com letra maiuscula
+	//Evite usar variaveis de um caracter
+	
+	String nomeCompleto = "nome";
+	
+	
+}
