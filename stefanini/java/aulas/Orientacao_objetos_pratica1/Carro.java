f82683b707beac4ec1086e35d8532fce96691895
cbkremer <106339191+cbkremer@users.noreply.github.com>
2022-06-17 15:20:15 -0300

    Update Carro.java

--- stefanini/java/aulas/Orientacao_objetos_pratica1/Carro.java
+++ stefanini/java/aulas/Orientacao_objetos_pratica1/Carro.java
@@ -1,1 +1,16 @@
+package Aulas.Orientacao_objetos_pratica1;
 
+public class Carro {
+    String marca;
+    String modelo;
+    int velocidade_max;
+    int aceleracao;
+    double km_litro;
+
+    void acelerar(){
+        System.out.println("Acelerando...");
+    }
+    void frear(){
+        System.out.println("Freando..");
+    }
+}
