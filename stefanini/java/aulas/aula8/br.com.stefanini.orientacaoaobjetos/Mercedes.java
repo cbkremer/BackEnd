db297c8fa2b870510a47c91be7fe484d214c607e
cbkremer <106339191+cbkremer@users.noreply.github.com>
2022-06-17 17:52:18 -0300

    Add files via upload

---
+++ stefanini/java/aulas/aula8/br.com.stefanini.orientacaoaobjetos/Mercedes.java
@@ -0,0 +1,12 @@
+package br.com.stefanini.orientacaoaobjetos;
+
+public class Mercedes extends Carro {
+	public Mercedes() {
+		this.setMarca("Mercedes");
+	}
+	@Override
+	public String acelerar() {
+		return "A Mercedes possúi uma aceleração diferente para cada modelo.";
+	}
+	
+}
